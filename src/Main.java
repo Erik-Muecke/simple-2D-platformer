122961e4064a0c2bd8eb068fe8b2c79db8b36757
Erik Mücke <erik.muecke@live.de>
2026-03-18 13:09:04 +0100

    added Game class which is created in the main method.

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,9 @@
-import javax.swing.SwingUtilities;
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.*;
 
 public class Main {
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {
-            JFrame frame = new JFrame("Basic JFrame");
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.setSize(800,600);
-            frame.setResizable(true); // Fenster ist skalierbar
-            frame.setLocationRelativeTo(null); // Zentriert auf dem Bildschirm
-            frame.setVisible(true); // zeigt das Fenster an
 
-        });
+    public static void main(String[] args) {
+        new Game();
     }
 }
