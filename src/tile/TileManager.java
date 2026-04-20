261e9a7d0a7c7a5230b61b5e8c9fa73f56964584
Erik Mücke <erik.muecke@live.de>
2026-04-20 19:40:06 +0200

    refactored and added error handling
    refactored tile image loading process; added error handling for missing ressources

--- src/tile/TileManager.java
+++ src/tile/TileManager.java
@@ -14,6 +14,9 @@ public class TileManager {
     int mapTileNum[][]; //2D Array um die Nummer der Kachel zu speichern, die an jeder Position gezeichnet werden soll
 
     public TileManager(GamePanel gp) {
+
+
+
         this.gp = gp;
 
         tile = new Tile[10]; //konstruiert 10 Tile Objekte im Tile Array
@@ -31,13 +34,13 @@ public class TileManager {
     try {
 
         tile[0] = new Tile();  //erstellt ein neues Tile Objekt an der Stelle 0 im Array
-        tile[0].image = ImageIO.read(getClass().getResourceAsStream("/tiles/grass.png"));  //laedt das Bild aus dem res Ordner
+        tile[0].image = loadTileImage("/tiles/grass.png");  //laedt das Bild aus dem res Ordner
 
         tile[1] = new Tile();
-        tile[1].image = ImageIO.read(getClass().getResourceAsStream("/tiles/wall.png"));
+        tile[1].image = loadTileImage("/tiles/wall.png");
 
         tile[2] = new Tile();
-        tile[2].image = ImageIO.read(getClass().getResourceAsStream("/tiles/earth.png"));
+        tile[2].image = loadTileImage("/tiles/earth.png");
 
     }catch(Exception e){
     System.out.println("Fehler beim Laden der Kachelbilder: " + e.getMessage());}
@@ -75,18 +78,31 @@ public class TileManager {
     public void loadMap() {
         //Hier wird die Karte geladen, indem die Nummer der Kachel für jede Position im mapTileNum Array festgelegt wird
         try {
-            InputStream is = getClass().getResourceAsStream("tilemap.txt");
-            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            InputStream is = getClass().getResourceAsStream("tilemap.txt"); //öffnet die Textdatei
+
+            if (is == null) {
+                throw new Exception("Ressource nicht gefunden: tilemap.txt"); //erstellt neue Exception, wenn der wert null ist
+
+
+            }
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(is)); //lädt die Textdatei in einen Buffered reader um sie zu benutzen
+
+
 
             int col = 0;
             int row = 0;
 
             while(col < GamePanel.MaxScreenCol && row < GamePanel.MaxScreenRow) {
                 String line = br.readLine(); //liest eine Zeile aus der Textdatei
+                if(line == null) {
+                    throw new IllegalStateException("Tilemap hat weniger als" + GamePanel.MaxScreenRow + " Zeilen."); //erstellt neue Exception, wenn die Zeile null ist, also das Ende der Datei erreicht ist, bevor alle Zeilen gelesen wurden
+                }
+                String numbers[] = line.trim().split("\\s+");
 
                 while(col < GamePanel.MaxScreenCol) {
 
-                    String numbers[] = line.split("  ");
+
 
                     int num = Integer.parseInt(numbers[col]); //Gelesene Zahl wird von String zu Integer konvertiert
 
@@ -104,5 +120,29 @@ public class TileManager {
         }catch(Exception e){
             System.out.println("Fehler beim Laden der Karte: " + e.getMessage());
         }
+    }
+
+    public java.awt.image.BufferedImage loadTileImage(String imagePath) {
+
+
+        try (InputStream is = getClass().getResourceAsStream(imagePath)) {
+            if  (is == null) {
+                throw new Exception("Ressource nicht gefunden: " + imagePath);
+            }
+            return ImageIO.read(is);
+        } catch (Exception e) {
+            System.out.println("Fehler beim Laden der Kachelbilder: " + e.getMessage());
+            try(InputStream no_image = getClass().getResourceAsStream("/missing/image_not_found.png")){
+                if (no_image == null) {
+                    throw new Exception("Ressource nicht gefunden: /missing/image_not_found.png");
+                }
+                return ImageIO.read(no_image);
+
+            }catch(Exception f){
+                System.out.println("Fehler beim Laden der Standardfehlermeldung: " + e.getMessage());
+                return null;
+            }
+        }
+
     }
 }
