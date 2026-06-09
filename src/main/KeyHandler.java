d9575a489c7093891626845fd340ba19e0b88f4e
Erik Mücke <erik.muecke@live.de>
2026-06-09 21:39:55 +0200

    FIX: now the pause screen should work

--- src/main/KeyHandler.java
+++ src/main/KeyHandler.java
@@ -56,7 +56,6 @@ public class KeyHandler implements KeyListener {
                     System.exit(0);
                 }
             }
-            commandNum = 0;
         }
 
         else if (gp.gameState == gp.playState) {
